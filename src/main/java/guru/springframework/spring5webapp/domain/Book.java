536aee23e5a97a7d94fbe102f744359a8d1846fd
aztecus1 <alaaeddine.ouerfelli@esprit.tn>
2021-09-27 16:29:44 +0100

    Adding Publisher class with an example of one to many. Including BootStrapData for publisher. Creation of publisher repository

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -10,6 +10,9 @@ public class Book {
     private Long id;
     private String title;
     private String isbn;
+    @ManyToOne
+    private Publisher publisher;
+
     @ManyToMany
     @JoinTable(name="author_book",joinColumns = @JoinColumn(name="book_id"),inverseJoinColumns = @JoinColumn(name= "author_id"))
     private Set<Author> authors=new HashSet<>();
@@ -21,6 +24,14 @@ public class Book {
         this.title = title;
         this.isbn = isbn;
 
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Long getId() {
